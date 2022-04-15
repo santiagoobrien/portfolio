3170ef1ff356c56351a2e386b6ae4646704742f7
santiagoobrien <santiago.ob@hotmail.com>
2022-04-15 19:37:28 -0300

    cambios 4

--- contacto.php
+++ contacto.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacto</title>
+    <title>Contacto prueba</title>
     <link rel="stylesheet" href="css/fontawesome-free-6.0.0-web/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome-free-6.0.0-web/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
